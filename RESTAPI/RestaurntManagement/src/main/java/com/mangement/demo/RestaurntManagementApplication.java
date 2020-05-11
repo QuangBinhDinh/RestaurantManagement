be472a61d3f8e50b7648fe976d1c1f01c1e1cb94
QuangBinhDinh <43164997+QuangBinhDinh@users.noreply.github.com>
2020-05-11 22:13:08 +0700

    Add files via upload

---
+++ RESTAPI/RestaurntManagement/src/main/java/com/mangement/demo/RestaurntManagementApplication.java
@@ -0,0 +1,13 @@
+package com.mangement.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurntManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurntManagementApplication.class, args);
+	}
+
+}
